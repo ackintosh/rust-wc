a1d34b32face4b4234dbbe1e9695fb961bfa5a55
akihito.nakano <sora.akatsuki@gmail.com>
2019-06-23 10:20:38 +0900

    Align output format

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::io::Read;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
     if args.len() != 2 {
         panic!("wrong number of arguments. can only 1 argument for now, sorry...");
     }
@@ -18,11 +17,15 @@ fn main() {
         .read_to_string(&mut contents)
         .expect("something went wrong reading the file");
 
-    println!("{}", contents);
-
     let lines: usize = contents.lines().count();
     let words: usize = contents.split_whitespace().count();
-    let size: usize = contents.len();
+    let bytes: usize = contents.len();
 
-    println!("lines: {}, words: {}, size: {}, filename: {}", lines, words, size, filename);
+    println!(
+        "{0: >7} {1: >7} {2: >7} {3: >7}",
+        lines,
+        words,
+        bytes,
+        filename
+    );
 }
