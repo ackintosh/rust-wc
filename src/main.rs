9c1931fc089ab67bdb32b5fce6edb3938cf7994f
akihito.nakano <sora.akatsuki@gmail.com>
2019-06-23 09:59:13 +0900

    Read a file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 use std::env;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
+    if args.len() != 2 {
+        panic!("wrong number of arguments. can only 1 argument for now, sorry...");
+    }
+
+    let filename = &args[1];
+
+    let mut file = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    file
+        .read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("{}", contents);
 }
